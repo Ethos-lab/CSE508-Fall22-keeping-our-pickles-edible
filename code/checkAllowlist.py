037f35a85dfbd45057f421b84f0745c8d443da81
shreejay23 <shreejay.va@gmail.com>
2022-10-29 00:49:26 -0400

    Modified Code to use list from config files

--- code/checkAllowlist.py
+++ code/checkAllowlist.py
@@ -7,9 +7,9 @@ import io
 import logging
 import pickle
 import torch
-
-
-
+import os
+_ALLOWLIST = []
+_SAFECLASS = []
 
 	
 def detect_pickle_allow_list(filePath):
@@ -24,10 +24,24 @@ def detect_pickle_allow_list(filePath):
 	
 	print('✅ FILE SEEMS TO BE SAFE')
 
+def detect_pickle_safe_class(className) -> bool:
+	if className not in _SAFECLASS:
+		return False
+	return True
 
 if __name__ == "__main__":
 	print('input file path')
 	filePath = input()
+ 
+	allowlist_file_path = os.path.join('code', 'config_files', 'allowlist.config')
+	allowlist_file_data = open(allowlist_file_path).read()
+ 
+	safeclass_file_path = os.path.join('code', 'config_files', 'safeclasses.config')
+	safeclass_file_data = open(safeclass_file_path).read()
+ 
+	_ALLOWLIST = allowlist_file_data.split('\n')
+	_SAFECLASS = safeclass_file_data.split('\n')
+ 
 	detect_pickle_allow_list(filePath)
 
 
